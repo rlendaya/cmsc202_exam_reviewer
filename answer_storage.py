603a857c4d766ef93323b5b0943b65bd58b9cb72
ecdego7 <ecdego@up.edu.ph>
2024-12-13 21:08:37 +0800

    Added Test Run, fixed bugs

--- answer_storage.py
+++ answer_storage.py
@@ -2,8 +2,6 @@
 #Jerry Luis
 #Ellen Dee
 
-import os
-
 '''
 save_answer:
 -called when taking the test proper, when user agrees to save answers
@@ -21,8 +19,7 @@ retrieve_answers:
 '''
 
 
-
-
+# removed OOP for now for fine tuning, please feel free to convert if needed
 #class AnswerStorage:
     #def __init__(self):
         #self.answers = {}  # Store user answers, key=question ID and value=answer
@@ -31,9 +28,10 @@ retrieve_answers:
 #     (question['id'], question['question'], question['correct_answer'], user_answer)
 
 
+import os
 
 ''''
-Expected input: a list of dictionaries with the following info: 
+def save_answers expected input: a list of dictionaries with the following info: 
 [
 {session_id:'session_id', question_id: 'question_id, subject: 'subject', type: 'type', correct_answer: correct_answer, user_answer: 'user_answer', question: 'question text'
 ]
@@ -43,15 +41,15 @@ sample:
 {'session_id': 1734082385, 'id': '12', 'subject': 'cmsc 206', 'type': 'multiplechoice', 'correct_answer': '3', 'user_answer': 3, 'question': 'It is the number of vertices in a graph.'}, 
 {'session_id': 1734082185, 'id': '19', 'subject': 'cmsc 206', 'type': 'multiplechoice', 'correct_answer': '3', 'user_answer': 4, 'question': 'It is the number of edges in a graph.'}]
 
+'''
 
-    '''
 def save_answers(answer_data):
-    # Parse and format answers in answer_data list
     current_session_id = None
 
     # Open the file where answer data will be stored
-    with open('answers.txt', 'a') as file:
+    with open('answers.txt', 'a') as file: 
 
+        # Parse and format answers in answer_data list
         for dictionary in answer_data:
             keys = list(dictionary.keys())
 
@@ -63,26 +61,26 @@ def save_answers(answer_data):
             correct_answer = dictionary[keys[4]]
             is_correct = (str(user_answer).lower() == str(correct_answer).lower()) # Checks correctness of user answer
 
-    # Ends the session and starts session if new session available
-    if session_id != current_session_id:
-        if current_session_id is not None:
-            file.write("==== End of Session ====\n\n") # End of session
+            # Ends the session and starts session if new session available
+            if session_id != current_session_id:
+                if current_session_id is not None:
+                    file.write("######### End of Session #########\n\n\n") # End of session
 
-        # Header as the session date
-        file.write(f"Session ID: {session_id}\n")
-        file.write(" ========================\n")
-        current_session_id = session_id
+                # Header as the session date
+                file.write(f"Session ID: {session_id}\n")
+                file.write("========================\n")
+                current_session_id = session_id
 
-    # Write question details in file
-    file.write(f"Question ID: {question_id}\n")            
-    file.write(f"Subject: {subject}\n")
-    file.write(f"Question: {question_text}\n")
-    file.write(f"Correct Answer: {correct_answer}\n")
-    file.write(f"User Answer: {user_answer}\n")
-    file.write(f"Is Correct: {is_correct}\n\n") # create an empty line in between questions
-    
-    # End session
-    file.write("==== End of Session ====\n\n")
+            # Write question details in file
+            file.write(f"Question ID: {question_id}\n")            
+            file.write(f"Subject: {subject}\n")
+            file.write(f"Question: {question_text}\n")
+            file.write(f"Correct Answer: {correct_answer}\n")
+            file.write(f"User Answer: {user_answer}\n")
+            file.write(f"Is Correct: {is_correct}\n\n") # create an empty line in between questions
+        
+        # End session
+        file.write("######### End of Session #########\n\n\n")
 
 
     '''
@@ -102,8 +100,9 @@ def save_answers(answer_data):
     User Answer: False
     Is Correct: False
 
-    ==== End of Session ====
+    ######### End of Session #########
 
+    
     Session Date: 1734082086
     ========================
     Question ID: 1
@@ -113,7 +112,7 @@ def save_answers(answer_data):
     User Answer: 4
     Is Correct: False
 
-    ==== End of Session ====
+    ######### End of Session #########
     '''
 
 
@@ -158,23 +157,22 @@ def retrieve_answers(file_name):
                 print(f"{idx + 1}. Session: {session}")
             
             choice = int(input("\nNumber: "))
+            print("")
             if choice == 0:
                 return # Return to Main Menu
             elif 1 <= choice <= len(sessions):
                 current_session = sessions[choice - 1]
-                print(f"current session: {current_session}")
 
                 # Display data of chosen session
                 session_found = False
                 for line in lines:
                     line = line.strip()
-                    if line.startswith(f"Session Date: {current_session}"):
+                    if line.startswith(f"Session ID: {current_session}"):
                         session_found = True # Flags session beginning
                     if session_found:
                         print(line) # Print subsequent lines of the chosen session
-                    if line == "==== End of Session ====":
+                    if line == "#### End of Session ####":
                         session_found = False # Reset flag
-                        print(f"\n{line}\n")
                         break # Stop loop
             
             else:
@@ -191,7 +189,7 @@ def retrieve_answers(file_name):
 
 
 
-# TEST RUN
+####################### TEST RUN #########################
 
 def test_main_menu():
     while True:
@@ -199,7 +197,8 @@ def test_main_menu():
         print("1. View Previous Sessions")
         print("5. Exit\n")
 
-        choice = input("Please choose a number from the main menu.\n")
+        choice = input("Please choose a number from the main menu: ")
+        print("")
 
 
         if choice == "1":
@@ -212,13 +211,17 @@ def test_main_menu():
 
 
 
-
-
-test_answer_data = [{'session_id': 1734082085, 'id': '13', 'subject': 'cmsc 206', 'type': 'multiplechoice', 'correct_answer': '3', 'user_answer': 3, 'question': 'It uniquely identifies each entity in the entity set'}, 
+test_answer_data1 = [{'session_id': 1734082085, 'id': '13', 'subject': 'cmsc 206', 'type': 'multiplechoice', 'correct_answer': '3', 'user_answer': 3, 'question': 'It uniquely identifies each entity in the entity set'}, 
 {'session_id': 1734082085, 'id': '12', 'subject': 'cmsc 206', 'type': 'multiplechoice', 'correct_answer': '3', 'user_answer': 3, 'question': 'It is the number of vertices in a graph.'}, 
 {'session_id': 1734082085, 'id': '19', 'subject': 'cmsc 206', 'type': 'multiplechoice', 'correct_answer': '3', 'user_answer': 4, 'question': 'It is the number of edges in a graph.'}]
 
-save_answers(test_answer_data) # creates answers.txt file that saves the answers 
+test_answer_data2 = [{'session_id': 1734082086, 'id': '14', 'subject': 'cmsc 203', 'type': 'multiplechoice', 'correct_answer': '3', 'user_answer': 3, 'question': 'It uniquely identifies each entity in the entity set'}, 
+{'session_id': 1734082086, 'id': '13', 'subject': 'cmsc 201', 'type': 'T/F', 'correct_answer': 't', 'user_answer': 'F', 'question': 'Order is the number of vertices in a graph.'}, 
+{'session_id': 1734082086, 'id': '20', 'subject': 'cmsc 202', 'type': 'multiplechoice', 'correct_answer': '3', 'user_answer': 4, 'question': 'It is the number of edges in a graph.'}]
+
+save_answers(test_answer_data1) # creates answers.txt file that saves the answers 
+
+save_answers(test_answer_data2)
 
 test_main_menu()
 

256651c0e9b5ca07806e73eee96a54da3a06d0a1
acjumaquio <acjumaquio@up.edu.ph>
2024-12-13 13:56:10 +0800

    Removed test questions

--- performance_analysis.py
+++ performance_analysis.py
@@ -9,34 +9,6 @@ class PerformanceAnalysis:
         self.wrong_count = 0
         self.results = [] 
 
-    def ask_question(self, question_data):
-        #Displays a question, gets the user's answer and check if it's correct
-        question = question_data['question']
-        correct_answer = question_data['answer']
-
-        #Display the question
-        print(f"\nQuestion: {question}") 
-
-        #Ask the user for their answer
-        user_answer = input("Your answer: ").strip()  # Strip removes the extra spaces
-
-        #Check if the answer provided is correct
-        if user_answer.lower() == correct_answer.lower(): #lower is used so the answers will be in the same lettercase
-            print("Correct!")
-            self.correct_count += 1
-            result = "Correct"
-        else:
-            print(f"Incorrect! The correct answer is: {correct_answer}")
-            self.wrong_count += 1
-            result = "Incorrect"
-
-        #Save the results for the summary
-        self.results.append({
-            "question": question,
-            "user_answer": user_answer,
-            "correct_answer": correct_answer,
-            "result": result 
-        })
 
     def performance_summary(self):
         #Shows a summary of the user's overall performance
@@ -69,23 +41,6 @@ class PerformanceAnalysis:
         for i, res in enumerate(self.results, 1):
             print(f"{i}.{res['question']:<60}{res['user_answer']:<18}{res['correct_answer']:<18}{res['result']:<15}")
         
-        # Show detailed results for each question answered
-        print("\nDetailed Results:")
-        for i, res in enumerate(self.results, 1):
-            print(f"{i}.{res['question']}")
-            print(f"    Your Answer: {res['user_answer']} | Correct Answer: {res['correct_answer']} |Result: {res['result']}")
-
-#Sample run
-questions = [
-    {"question": "What is the powerhouse of the cell?", "answer": "Mitochondria"},
-    {"question": "What is 1 + 1?", "answer": "4"},
-    {"question": "Who is our national hero?", "answer": "Jose Rizal"}
-]
-
 # Create an instance of the PerformanceAnalysis class
 performance_analyzer = PerformanceAnalysis() 
 
-for q in questions:
-    performance_analyzer.ask_question(q)
-
-performance_analyzer.performance_summary()
